9c8226c85fc4539393875039b716aeee6a0cda6e
cynicjj <cynicjj@DESKTOP-IR5AFJB>
2023-10-28 16:40:34 +0900

    static 제거 준비

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
@@ -1,5 +1,17 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.craftedsw.tripservicekata.exception.CollaboratorCallException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
 public class TripDAOTest {
 
+	@Test
+	void 테스트에서_호출하면_예외를_던짐() {
+		assertThrows(CollaboratorCallException.class, () -> {
+			new TripDAO().tripsBy(new User());
+		});
+	}
 }
