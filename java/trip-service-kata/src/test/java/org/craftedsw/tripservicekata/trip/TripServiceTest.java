b89a8bffd9045a8e84ae4cddf06d0efa9aef538c
cynicjj <cynicjj@DESKTOP-IR5AFJB>
2023-10-28 14:55:19 +0900

    UserBuilder

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TripServiceTest {
@@ -14,49 +15,51 @@ public class TripServiceTest {
 	private static final User GUEST = null;
 	private static final User REGISTERED_USER = new User();
 	private static final User ANOTHER_USER = new User();
-	
+
+	private static final Trip TO_BRAZIL = new Trip();
+	private static final Trip TO_KOREA = new Trip();
+
 	private User loggedInUser;
+	private TripService tripService;
+
+	@BeforeEach
+	void init() {
+		tripService = new TestableTripService();
+		loggedInUser = REGISTERED_USER;
+	}
 
 	@Test
 	void 로그인_유저가_없으면_예외() {
-		TripService tripService = new TestableTripService();
 		loggedInUser = GUEST;
-		
+
 		assertThrows(UserNotLoggedInException.class, () -> {
 			tripService.getTripsByUser(new User());
 		});
 	}
-	
+
 	@Test
 	void 친구가_아니면_여행목록_없다() {
-		TripService tripService = new TestableTripService();
-		loggedInUser = REGISTERED_USER;
-		
-		User targetUser = new User();
-		targetUser.addFriend(ANOTHER_USER);
-		targetUser.addTrip(new Trip());
+		User targetUser = UserBuilder.aUser()
+				.friendsWith(ANOTHER_USER)
+				.tripsWith(TO_BRAZIL)
+				.build();
 
 		List<Trip> trips = tripService.getTripsByUser(targetUser);
-		
+
 		assertTrue(trips.isEmpty());
 	}
-	
+
 	@Test
 	void 친구의_여행목록을_가져온다() {
-		TripService tripService = new TestableTripService();
-		loggedInUser = REGISTERED_USER;
-
-		User targetUser = new User();
-		targetUser.addFriend(ANOTHER_USER);
-		targetUser.addFriend(loggedInUser);
-		targetUser.addTrip(new Trip());
-		targetUser.addTrip(new Trip());
-		targetUser.addTrip(new Trip());
+		User targetUser = UserBuilder.aUser()
+				.friendsWith(ANOTHER_USER, loggedInUser)
+				.tripsWith(TO_BRAZIL, TO_KOREA)
+				.build();
 
 		List<Trip> trips = tripService.getTripsByUser(targetUser);
 
-		assertTrue(trips.size() == 3);
-		
+		assertTrue(trips.size() == 2);
+
 	}
 
 	private class TestableTripService extends TripService {
@@ -65,7 +68,7 @@ public class TripServiceTest {
 		protected User getLoggedInUser() {
 			return loggedInUser;
 		}
-		
+
 		@Override
 		protected List<Trip> tripsByUser(User user) {
 			return user.trips();
