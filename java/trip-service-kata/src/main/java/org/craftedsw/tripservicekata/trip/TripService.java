2b0e18581ca13968f9566f25196ef5706743f0c8
cynicjj <cynicjj@DESKTOP-IR5AFJB>
2023-10-31 07:56:08 +0900

    : TripService
    주석 처리한 임포트
    
    learn Test 레드바

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -11,7 +11,9 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
+
 		User loggedUser = UserSession.getInstance().getLoggedUser();
+
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -20,13 +22,14 @@ public class TripService {
 					break;
 				}
 			}
+
 			if (isFriend) {
 				tripList = TripDAO.findTripsByUser(user);
 			}
+
 			return tripList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
 	}
-	
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,23 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.jupiter.api.Test;
+
+/*
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import static org.mockito.BDDMockito.given;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+//*/
+
 public class TripServiceTest {
-	
+
+	@Test
+	void learn() {
+		TripService service = new TripService();
+		service.getTripsByUser(null);
+	}
 }
